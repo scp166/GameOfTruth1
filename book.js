c33d1ff6a4ad013adbea45b4e81f3f4dbd39f3e1
scp166 <1653817310@qq.com>
2021-11-22 19:44:49 +0800

    new Code

---
+++ book.js
@@ -0,0 +1,11 @@
+module.exports = {
+    title: "书名",
+    description: "描述",
+    isban: "编号",
+    author: "作者",
+    lang: "zh-cn",
+    plugins: ["-lunr", "-search", "search-pro-ling"],
+    pluginConfig: {
+
+    }
+}
